048e8c0b87e3550af469556ff4301b9a1499a507
Renanideiatecnologia <renan@ideiatecnologia.com.br>
2021-10-25 17:19:07 -0300

    Incluindo mensagem

--- ideianotifier.go
+++ ideianotifier.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"ideianotifier/repository"
 	usecases "ideianotifier/useCases"
 )
 
 func main() {
+	fmt.Println("Verificando estoque pendente")
 	pendingQty := repository.GetPendingQty()
 	companyName := repository.GetCompanyName()
 	usecases.SendNotification(companyName, pendingQty)
